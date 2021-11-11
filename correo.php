8c33b9cf2e4b3d96c65815319d142d12bea1f61c
EMMANUELSCH19 <emmanuel_1320114041@uptecamac.edu.mx>
2021-11-10 18:27:43 -0600

    a

--- correo.php
+++ correo.php
@@ -4,8 +4,8 @@ $destino = "emmanuel_1320114041@uptecamac.edu.mx";
 $nombre = $_POST["nombre"];
 $correo = $_POST["correo"];
 $celular = $_POST["celular"];
-$mensaje = $_POST["mensaje"];
-$contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nCelular: " . $celular . "\nMensaje: " . $mensaje;
+// $mensaje = $_POST["mensaje"];
+$contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nCelular: " . $celular;
 
 mail($destino, "Contacto", $contenido);
 header("Location:index.php");
